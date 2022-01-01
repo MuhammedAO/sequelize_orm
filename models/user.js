0614b9a91c4d55a956ba00a2743dc932869d9fdd
Muhammed ogunsanya <mspeaks910@gmail.com>
2022-01-01 01:49:32 +0100

    validation

--- models/user.js
+++ models/user.js
@@ -24,15 +24,28 @@ module.exports = (sequelize, DataTypes) => {
     },
     name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {msg: 'Field cannot be null'},
+        notEmpty: {msg: 'Field cannot be empty'}
+      }
     },
     email: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {msg: 'Field cannot be null'},
+        notEmpty: {msg: 'Field cannot be empty'},
+        isEmail: {msg: 'Must be a valid email'}
+      }
     },
     role: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {msg: 'Field cannot be null'},
+        notEmpty: {msg: 'Field cannot be empty'}
+      }
     }
   }, {
     sequelize,
