2b239882a7e118c63d308d445ee62d60ae18d90f
Muhammed ogunsanya <mspeaks910@gmail.com>
2022-01-01 01:49:39 +0100

    crud

--- app.js
+++ app.js
@@ -43,7 +43,8 @@ const {userUuid, body} = req.body
 try {
 
   const user = await User.findOne({where: {
-    uuid: userUuid
+    uuid: userUuid,
+    include: 'posts'
   }})
   const post = await Post.create({body, userId: user.id})
   return res.json(post)
@@ -61,6 +62,39 @@ app.get('/posts', async (req, res) => {
     console.log(error)
     return res.status(500).json({error: 'Something went wrong'})
   }
+  })
+
+  app.delete('/users/:uuid', async (req, res) => {
+    const uuid = req.params.uuid
+    try {
+      const user = await User.findOne({ where: { uuid } })
+  
+      await user.destroy()
+  
+      return res.json({ message: 'User deleted!' })
+    } catch (err) {
+      console.log(err)
+      return res.status(500).json({ error: 'Something went wrong' })
+    }
+  })
+  
+  app.put('/users/:uuid', async (req, res) => {
+    const uuid = req.params.uuid
+    const { name, email, role } = req.body
+    try {
+      const user = await User.findOne({ where: { uuid } })
+  
+      user.name = name
+      user.email = email
+      user.role = role
+  
+      await user.save()
+  
+      return res.json(user)
+    } catch (err) {
+      console.log(err)
+      return res.status(500).json({ error: 'Something went wrong' })
+    }
   })
   
 
